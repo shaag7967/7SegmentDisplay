074a2ba332d6b007fea60221987c18f449bf7a84
seven <shaag@gmx.net>
2017-12-12 22:00:27 +0100

    set config by i2c working

---
+++ firmware/7SegmentDisplay.X/src/i2c.c
@@ -0,0 +1,6 @@
+
+#include "i2c.h"
+#include <xc.h>
+
+
+
---
+++ firmware/7SegmentDisplay.X/src/i2c.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   i2c.h
+ * Author: seven
+ *
+ * Created on 9. Dezember 2017, 23:52
+ */
+
+#ifndef I2C_H
+#define	I2C_H
+
+
+
+#endif	/* I2C_H */
+
